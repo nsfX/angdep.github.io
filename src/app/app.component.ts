33cd3c9fe276cf03ed2e4640886a624e0420b974
nake0x <ryukmyl@hotmail.cl>
2021-09-17 02:46:18 -0300

    ultimo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { ApiserviceService } from './apiservice.service';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myApp';
-  //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
+  constructor(private api: ApiserviceService) { }
+
+  ngOnInit(): void {
+    console.log("entrada")
+    this.api.getResponse().subscribe(result => {
+      console.log(result);
+    });
+  }
 }
